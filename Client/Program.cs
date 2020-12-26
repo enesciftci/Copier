315f02c370b670b478c6365e714996577362379d
İbrahim Enes Çiftçi <ciftcienes23@hotmail.com>
2020-12-26 19:42:06 +0300

    Added FileSystemGlobing package and file matching

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 
 namespace Client
 {
@@ -6,7 +10,22 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Copier help");
+            }
+            else
+            {
+                Matcher matcher = new Matcher(StringComparison.InvariantCulture);
+                matcher.AddInclude(args[1]);
+                var directoryInfo = new DirectoryInfo(args[0]);
+                var directoryWrapper = new DirectoryInfoWrapper(directoryInfo);
+                var files = matcher.Execute(directoryWrapper).Files;
+
+                Console.WriteLine(files.Select(p => p.Path).Aggregate((a, b) => a + ", " + b));
+                Console.ReadKey();
+            }
+
         }
     }
 }
