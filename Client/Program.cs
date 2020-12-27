c62160b096fc561db6b2e70982545bcdfe109301
İbrahim Enes Çiftçi <ciftcienes23@hotmail.com>
2020-12-27 18:11:13 +0300

    CommandLineParser library added CommandOptions.cs Added

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Channels;
+using CommandLine;
 using Microsoft.Extensions.FileSystemGlobbing;
 using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 
@@ -10,22 +14,38 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Copier help");
-            }
-            else
-            {
-                Matcher matcher = new Matcher(StringComparison.InvariantCulture);
-                matcher.AddInclude(args[1]);
-                var directoryInfo = new DirectoryInfo(args[0]);
-                var directoryWrapper = new DirectoryInfoWrapper(directoryInfo);
-                var files = matcher.Execute(directoryWrapper).Files;
+            Parser.Default.ParseArguments<CommandOptions>(args)
+                .WithParsed(StartWatching)
+                .WithNotParsed(a =>
+                {
+                    Environment.Exit(1);
+                });
+        }
 
-                Console.WriteLine(files.Select(p => p.Path).Aggregate((a, b) => a + ", " + b));
-                Console.ReadKey();
-            }
+        private static void StartWatching(CommandOptions options)
+        {
+            var files = GetMatchingFiles(options);
+            var file = "";
+            var watcher = new FileSystemWatcher
+            {
+                Path = options.SourceDirectoryPath,
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName,
+                Filter = file
+            };
+            watcher.Changed += (sender, args) => Console.WriteLine("File has changed");
+            watcher.Renamed += (sender, args) => Console.WriteLine("File has renamed");
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine(files.Select(p => p.Path).Aggregate((a, b) => a + ", " + b));
+        }
 
+        private static IEnumerable<FilePatternMatch> GetMatchingFiles(CommandOptions options)
+        {
+            Matcher matcher = new Matcher(StringComparison.InvariantCulture);
+            matcher.AddInclude(options.FileGlobPattern);
+            var directoryInfo = new DirectoryInfo(string.IsNullOrWhiteSpace(options.SourceDirectoryPath) ? Directory.GetCurrentDirectory() : options.SourceDirectoryPath);
+            var directoryInfoWrapper = new DirectoryInfoWrapper(directoryInfo);
+            var files = matcher.Execute(directoryInfoWrapper).Files;
+            return files;
         }
     }
 }
